496e3f50a518497b5099b8c02d2f69798ea6ba1a
David Mwendwa <klatzdavie19@gmail.com>
2021-06-20 17:00:05 +0300

    feat: passport setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
