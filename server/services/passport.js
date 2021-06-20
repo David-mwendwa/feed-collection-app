37cf3f84ba78dbf947b98a132d1bdbbda52c21d0
David Mwendwa <klatzdavie19@gmail.com>
2021-06-20 23:35:43 +0300

    feat: encode and deserialize user

--- server/services/passport.js
+++ server/services/passport.js
@@ -7,6 +7,16 @@ dotenv.config({ path: './config.env' });
 
 const User = mongoose.model('User');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
