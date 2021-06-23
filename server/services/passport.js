5061c7376abf475e507e31ddcd07f21b85bd7195
David Mwendwa <klatzdavie19@gmail.com>
2021-06-23 22:09:04 +0300

    refactor: refactoring googleStategy with async await

--- server/services/passport.js
+++ server/services/passport.js
@@ -26,16 +26,17 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        // We already have a record with the given profile ID
+        done(null, existingUser);
+      } else {
+        // We dont have a record with this ID, make a new record
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
